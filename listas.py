ac0ca611de8f0226d8587a4341ff7f37fdf4315a
Yuliana <ya.aldrich@yahoo.com>
2021-04-08 01:38:08 +0000

    listas

---
+++ listas.py
@@ -0,0 +1,30 @@
+lista1=['s', 'e', 'a', 'C','C', 'n', 'n', 'A']
+#print(len(lista1))
+#print(lista1[1:3])
+#print(lista1[:3])
+#print(lista1[4:])
+#print(lista1[-1])
+#print(lista1[-2:])
+#print(lista1[:-3])
+#lista = [0,1,2,3,4,5,6,7,8,9,'MITTEN',10,11,2,2]
+#lista2 = ['Z', 'X', 'P']
+#print(lista)
+#print(lista.count(10))
+#print(lista.index('MITTEN'))
+#print(lista.index(2,10))
+#lista.reverse()
+#print(lista)
+#lista2.sort()
+#print(lista2)
+#lista.pop(4)
+#print(lista)
+#lista2.insert(0, 'Y')
+#print(lista2)
+#lista3-lista2[:]
+#print(lista3)
+#del lista3[1:3]
+#print(lista3)
+#lista{12 ='MITTEN'}
+#print(lista)
+#lista.remove(0)
+#print(lista)
